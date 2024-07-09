ce562e3480f02ae21c9cce9c905c42acd6e8fcd6
apple <apple@gmail.com>
2024-07-09 17:44:20 +0900

    refactor: 사용안하는 코드 주석

--- task/src/test/java/com/terra/task/cpu/util/DataSQLGenerator.java
+++ task/src/test/java/com/terra/task/cpu/util/DataSQLGenerator.java
@@ -1,11 +1,5 @@
 package com.terra.task.cpu.util;
 
-import static com.terra.task.cpu.util.RandomValueGenerator.generateRandomValue;
-
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Random;
@@ -20,35 +14,35 @@ public class DataSQLGenerator {
   private static final Random RANDOM = new Random();
   private static final String OUTPUT_FILE_PATH = "src/test/resources/db/data.sql";
 
-  public static void main(String[] args) {
-    try (BufferedWriter writer = new BufferedWriter(new FileWriter(OUTPUT_FILE_PATH))) {
-      writer.write("-- data.sql\n\n");
-      writer.write("TRUNCATE TABLE cpu_usage;\n\n");
-      writer.write("INSERT INTO cpu_usage (timestamp, cpu_percentage)\nVALUES\n");
-
-      LocalDateTime currentDate = START_DATE;
-      StringBuilder values = new StringBuilder();
-
-      while (!currentDate.isAfter(END_DATE)) {
-        BigDecimal cpuUsage = generateRandomValue(RANDOM);
-        String timestamp = currentDate.format(DATE_TIME_FORMATTER);
-        values.append(String.format("('%s', %.1f),\n", timestamp, cpuUsage));
-
-        currentDate = currentDate.plusMinutes(1);
-      }
-
-      int lastCommaIndex = values.lastIndexOf(",");
-      if (lastCommaIndex != -1) {
-        values.deleteCharAt(lastCommaIndex);
-      }
-
-      writer.write(values.toString());
-      writer.write(";\n");
-
-      System.out.println("data.sql file has been generated at " + OUTPUT_FILE_PATH);
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-  }
+//  public static void main(String[] args) {
+//    try (BufferedWriter writer = new BufferedWriter(new FileWriter(OUTPUT_FILE_PATH))) {
+//      writer.write("-- data.sql\n\n");
+//      writer.write("TRUNCATE TABLE cpu_usage;\n\n");
+//      writer.write("INSERT INTO cpu_usage (timestamp, cpu_percentage)\nVALUES\n");
+//
+//      LocalDateTime currentDate = START_DATE;
+//      StringBuilder values = new StringBuilder();
+//
+//      while (!currentDate.isAfter(END_DATE)) {
+//        BigDecimal cpuUsage = generateRandomValue(RANDOM);
+//        String timestamp = currentDate.format(DATE_TIME_FORMATTER);
+//        values.append(String.format("('%s', %.1f),\n", timestamp, cpuUsage));
+//
+//        currentDate = currentDate.plusMinutes(1);
+//      }
+//
+//      int lastCommaIndex = values.lastIndexOf(",");
+//      if (lastCommaIndex != -1) {
+//        values.deleteCharAt(lastCommaIndex);
+//      }
+//
+//      writer.write(values.toString());
+//      writer.write(";\n");
+//
+//      System.out.println("data.sql file has been generated at " + OUTPUT_FILE_PATH);
+//    } catch (IOException e) {
+//      e.printStackTrace();
+//    }
+//  }
 
 }
