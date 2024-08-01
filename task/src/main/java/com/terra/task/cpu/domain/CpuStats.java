ed6ba1258580ae870598349e17920163e6bc8ec6
gyuhwan <rbghks19@naver.com>
2024-08-01 11:27:46 +0900

    refactor: readme 위치 수정

--- task/src/main/java/com/terra/task/cpu/domain/CpuStats.java
+++
@@ -1,9 +0,0 @@
-package com.terra.task.cpu.domain;
-
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
-
-public record CpuStats(LocalDateTime date, BigDecimal maxUsage, BigDecimal minUsage,
-                       BigDecimal avgUsage) {
-
-}
